f434b199ab9c678de41b74e82dc587fd5148a98f
megan schulte <megan.schulte.pdx@gmail.com>
2019-04-29 20:02:34 -0700

    so much man

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,67 +1,89 @@
-using System;
-using System.Collections.Generic;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 
-namespace ToDoList.Models
+namespace ToDoList
 {
-
   public class Program
   {
-    public static void Main()
+    public static void Main(string[] args)
     {
-      Console.WriteLine("Welcome, would you like to add a new item to your list? (Y/N)");
-      string userResponse = Console.ReadLine();
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
 
-      if (userResponse == "Y")
-      {
+      host.Run();
+    }
+  }
+}
 
-      Console.WriteLine("Add new Item to your To-Do List:");
 
-      string description = Console.ReadLine();
-
-      Item newItem = new Item(description);
-      List<Item> newList = new List<Item> {newItem};
-      int x = 0;
-      for (int i = 0; x == 0; i++)
-      {
-        Console.WriteLine("Would you like to add another item or view list? (Add/View/Q)");
-
-        string answer = Console.ReadLine();
-
-        if (answer == "Add")
-        {
-          Console.WriteLine("Add new Item to your To-Do List:");
-
-          string description2 = Console.ReadLine();
-
-          Item newItem2 = new Item(description2);
-          // List<Item> newList = new List<Item> {newItem};
-        }
-        else if (answer == "View")
-        {
-          List<Item> result = Item.GetAll();
-          Console.WriteLine("Here is your list:");
-          foreach (Item thisItem in result)
-          {
-            Console.WriteLine(thisItem.GetDescription());
-          }
-        }
-        else if (answer == "Q")
-        {
-          x =1;
-          Console.WriteLine("Goodbye");
-          break;
-        }
-        else
-        {
-          Console.WriteLine("Please enter a valid choice.");
-        }
-      }
-
-     }
-     else if (userResponse == "N")
-     {
-       Console.WriteLine("Goodbye");
-     }
+// namespace ToDoList.Models
+// {
+//
+//   public class Program
+//   {
+//     public static void Main()
+//     {
+//       Console.WriteLine("Welcome, would you like to add a new item to your list? (Y/N)");
+//       string userResponse = Console.ReadLine();
+//
+//       if (userResponse == "Y")
+//       {
+//
+//       Console.WriteLine("Add new Item to your To-Do List:");
+//
+//       string description = Console.ReadLine();
+//
+//       Item newItem = new Item(description);
+//       List<Item> newList = new List<Item> {newItem};
+//       int x = 0;
+//       for (int i = 0; x == 0; i++)
+//       {
+//         Console.WriteLine("Would you like to add another item or view list? (Add/View/Q)");
+//
+//         string answer = Console.ReadLine();
+//
+//         if (answer == "Add")
+//         {
+//           Console.WriteLine("Add new Item to your To-Do List:");
+//
+//           string description2 = Console.ReadLine();
+//
+//           Item newItem2 = new Item(description2);
+//           // List<Item> newList = new List<Item> {newItem};
+//         }
+//         else if (answer == "View")
+//         {
+//           List<Item> result = Item.GetAll();
+//           Console.WriteLine("Here is your list:");
+//           foreach (Item thisItem in result)
+//           {
+//             Console.WriteLine(thisItem.GetDescription());
+//           }
+//         }
+//         else if (answer == "Q")
+//         {
+//           x =1;
+//           Console.WriteLine("Goodbye");
+//           break;
+//         }
+//         else
+//         {
+//           Console.WriteLine("Please enter a valid choice.");
+//         }
+//       }
+//
+//      }
+//      else if (userResponse == "N")
+//      {
+//        Console.WriteLine("Goodbye");
+//      }
+// }
+// }
+// }
 
       // Console.WriteLine("Add new Item to your To-Do List:");
       //
@@ -77,6 +99,3 @@ namespace ToDoList.Models
       //   Console.WriteLine(thisItem.GetDescription());
       //
       // }
-    }
-  }
-}
