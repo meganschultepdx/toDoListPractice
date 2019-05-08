1e6c7f67c811d8e5b543c0c9b50c05007bfb7333
Liz Kelley and megan schulte <megan@>
2019-05-08 16:33:19 -0700

    add Delete function to items.cs and matching test

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -207,6 +207,24 @@ namespace ToDoList.Models
       }
     }
 
+    public void Delete(int id)
+    {
+      MySqlConnection conn = DB.Connection();
+      conn.Open();
+      var cmd = conn.CreateCommand() as MySqlCommand;
+      cmd.CommandText = @"DELETE FROM items WHERE id = @thisId;";
+      MySqlParameter thisId = new MySqlParameter();
+      thisId.ParameterName = "@thisId";
+      thisId.Value = _id;
+      cmd.Parameters.Add(thisId);
+      cmd.ExecuteNonQuery();
+      conn.Close();
+      if (conn != null)
+      {
+        conn.Dispose();
+      }
+    }
+
     public int GetCategoryId()
     {
       return _categoryId;
